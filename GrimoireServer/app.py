37707ba2c040b2c01ee3fec055a29028e7f22fe3
Matt-fmllc <matt.fmllc@gmail.com>
2019-02-01 20:24:48 -0800

    Added Python Web backend Project Added RESTful interface for Character Data ie: Facade Added Service Layer (Dummy for now)

---
+++ GrimoireServer/app.py
@@ -0,0 +1,28 @@
+"""
+This script runs the application using a development server.
+It contains the definition of routes and views for the application.
+"""
+
+from flask import Flask
+
+
+
+#Global App 
+app = Flask(__name__)
+
+# Make the WSGI interface available at the top level so wfastcgi can get it.
+wsgi_app = app.wsgi_app
+
+# addition APIs
+import Character_Facade
+from Character_Facade import *
+
+
+if __name__ == '__main__':
+    import os
+    HOST = os.environ.get('SERVER_HOST', 'localhost')
+    try:
+        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+    except ValueError:
+        PORT = 5555
+    app.run(HOST, PORT)
